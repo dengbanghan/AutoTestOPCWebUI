c57fffa97be0973e65cf5c489d8a992dbcd87821
banghan.deng <dengbanghan@gmail.com>
2022-05-07 17:23:07 +0800

    判断测试报告目录是否存在，若是不存在则自动创建报告目录

--- allTest.py
+++ allTest.py
@@ -13,18 +13,23 @@ import unittest
 def allTests():
     '''获取所有需要执行的测试用例'''
     suite = unittest.defaultTestLoader.discover(
-        start_dir = os.path.join(os.path.dirname(__file__ ),'testCase'),
-        pattern = 'test_004*.py',
-        top_level_dir = None
+        start_dir=os.path.join(os.path.dirname(__file__), 'testCase'),
+        pattern='test_001*.py',
+        top_level_dir=None
     )
     return suite
 
 def getNowTime():
     '''获取当前的时间'''
-    return time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()))
+    # 精确到秒的时间格式：%Y-%m-%d %H_%M_%S
+    return time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
 if __name__ == '__main__':
-    fileName = os.path.join(os.path.dirname(__file__), 'report','EddidTestReport_{0}.html'.format(getNowTime()))
+
+    report_path = os.path.join(os.path.dirname(__file__), 'report')
+    if not os.path.exists(report_path):  # 创建报告目录
+        os.makedirs(report_path)
+    fileName = os.path.join(os.path.dirname(__file__), 'report', 'EddidTestReport_{0}.html'.format(getNowTime()))
     with open(fileName, 'wb+') as file:
         runner = HTMLTestRunnerNew.HTMLTestRunner(
             file,
